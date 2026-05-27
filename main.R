2fba86586f3c8fda17f6ec79360d626cecb3ff27
Dantz Farrow <dantzfarrow@gmail.com>
2026-05-27 14:34:35 -0500

    Annotate files

--- main.R
+++ main.R
@@ -2,14 +2,18 @@
 # Title: Main File for Pipeline
 # Author: Dantz Farrow
 # Last Modified: 05/26/2026
+# Notes: For troubleshooting, follow code-block annotations.
 ####################################
 
 # --- 1. Load Packages, Paths, and Raw Data
+### Populate Raw Datasets
 source("scripts/01_setup.R")
 
 # --- 2. Examine Data
 
 # --- 3. Clean Data (NCES & SEDA)
+### Output: Transformed Datasets
 source("scripts/03a_nces-clean.R")
+# source("scripts/03b_seda-clean.R")
 
 # --- 4. Consolidate Dataset
--- scripts/03a_nces-clean.R
+++ scripts/03a_nces-clean.R
@@ -1,7 +1,7 @@
 ####################################
 # Title: Clean Data (NCES)
 # Author: Dantz Farrow
-# Last Modified: 05/26/2026
+# Last Modified: 05/27/2026
 ####################################
 
 # --- Pre-processing ---
@@ -9,41 +9,46 @@ nces_proc <- nces_raw %>%
   # drop unnecessary columns
   select(1, 26, 15:25, 4:14) %>%
   # modify column names
+  # rename_with(., .fn, .cols) 
   rename_with(~paste0("locale_", str_sub(.x, -7, -1)), starts_with("Locale")) %>% 
   rename_with(~paste0("enrollment_", str_sub(.x, -7, -1)), starts_with("Total")) %>% 
-  rename_with(~paste0("fips_full"), starts_with("Agency ID")) %>% 
+  rename(fips_full = starts_with("Agency ID")) %>% 
   rename(district_name = "Agency Name")
 
 # --- Pivot & Sub NULL Values ---
 nces_long <- nces_proc %>% 
-  # pivot processed data on enrollment
+  # pivot: year, enrollment
   pivot_longer(
     cols = starts_with("enrollment"),
     names_to = "year",
-    names_prefix = "enrollment_",
+    names_prefix = "enrollment_", # strips prefix
     values_to = "enrollment"
   ) %>%
-  # substitute NAs and cast to numeric
+  # sub NAs
   mutate(enrollment = na_if(enrollment, "†")) %>% 
+  # cast numeric
   mutate(enrollment = as.numeric(enrollment))
 
 locale_long <- nces_proc %>% 
-  # pivot locales into new df
+  # pivot: year, locale
   pivot_longer(
     cols = starts_with("locale"),
     names_to = "year",
     names_prefix = "locale_",
     values_to = "locale"
   ) %>% 
+  # sub NAs
   mutate(locale = na_if(locale, "†")) %>% 
+  # fips_full + year = join key
   select(fips_full, year, locale)
 
-nces_clean <- nces_long %>% 
+nces_transform <- nces_long %>% 
   # drop locale wide cols
-  select(1:2, 14:15) %>% 
+  select(district_name, fips_full, year, enrollment) %>% 
   # rejoin locale_long
   left_join(locale_long, by = c("fips_full", "year"))
 
-# --- Export Dataset & Clean Up Env ---
-write_csv(nces_clean, file.path(TRANSFORMED, "nces_final.csv"))
+# --- Export Dataset/Clean Up Env ---
+write_csv(nces_transform, file.path(TRANSFORMED, "nces_transform.csv"))
+saveRDS(nces_transform, file.path(TRANSFORMED, "nces_transform.rds"))
 rm(locale_long, nces_long, nces_proc, nces_raw)
